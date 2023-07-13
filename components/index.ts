cb113eb03b4a2f8c2eaef4dfcf58ac34d57efb61
Sanjay Darshan <sanjay.darshan@ecstvm.local>
2023-07-13 20:30:54 +0530

    added filters in hero page

--- components/index.ts
+++ components/index.ts
@@ -1,19 +1,19 @@
 // import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
-// import CustomFilter from "./CustomFilter";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
 // import ShowMore from "./ShowMore";
-// import SearchBar from "./Searchbar";
+import SearchBar from "./SearchBar";
 import Hero from "./Hero";
 
 export {
   Hero,
   //   CarCard,
   CustomButton,
-  //   CustomFilter,
+  CustomFilter,
   Footer,
   NavBar,
   //   ShowMore,
-  //   SearchBar,
+  SearchBar,
 };
